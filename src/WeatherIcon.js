e85022ae02da97942af760df08c375f4cd556d25
Dileep <dileepmaurya800@gmail.com>
2022-12-04 18:53:18 +0530

    first commit

---
+++ src/WeatherIcon.js
@@ -0,0 +1,22 @@
+const iconPath = 'https://openweathermap.org/img/wn';
+
+export default {
+  '01d': `${iconPath}/01d@2x.png`,
+  '01n': `${iconPath}/01n@2x.png`,
+  '02d': `${iconPath}/02d@2x.png`,
+  '02n': `${iconPath}/02n@2x.png`,
+  '03d': `${iconPath}/03d@2x.png`,
+  '03n': `${iconPath}/03n@2x.png`,
+  '04d': `${iconPath}/04d@2x.png`,
+  '04n': `${iconPath}/04n@2x.png`,
+  '09d': `${iconPath}/09d@2x.png`,
+  '09n': `${iconPath}/09n@2x.png `,
+  '10d': `${iconPath}/10d@2x.png`,
+  '10n': `${iconPath}/10n@2x.png`,
+  '11d': `${iconPath}/11d@2x.png`,
+  '11n': `${iconPath}/11n@2x.png`,
+  '13d': `${iconPath}/13d@2x.png`,
+  '13n': `${iconPath}/13n@2x.png`,
+  '50d': `${iconPath}/50d@2x.png`,
+  '50n': `${iconPath}/50n@2x.png`,
+};
